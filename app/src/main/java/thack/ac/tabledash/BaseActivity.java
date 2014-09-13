71701d93e5f9e2b15a49c94e644e2876512c81a6
Zhu Liang <zhuliangg11@gmail.com>
2014-09-13 17:46:56 +0800

    set up check in dialog, basic server communication implementation

--- app/src/main/java/thack/ac/tabledash/BaseActivity.java
+++ app/src/main/java/thack/ac/tabledash/BaseActivity.java
@@ -1,8 +1,11 @@
 package thack.ac.tabledash;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
@@ -15,11 +18,21 @@ import android.nfc.tech.NfcA;
 import android.nfc.tech.NfcB;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 public class BaseActivity extends Activity {
@@ -29,6 +42,16 @@ public class BaseActivity extends Activity {
     public NfcAdapter mNfcAdapter;
     public BroadcastReceiver receiver;
     public IntentFilter filter;
+
+    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+
+    //Service Handler
+    ServiceHandler sh = new ServiceHandler();
+
+    /**
+     * Urls for server communications
+     */
+    static String CHECK_IN_URL = "http://tabledash.ml/checkin.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,7 +108,59 @@ public class BaseActivity extends Activity {
         String tag_ID = handleIntent(intent);
         Log.d(TAG, "ID:" + tag_ID);
         if(tag_ID != null){
-            Toast.makeText(this, "Tag ID: " + tag_ID, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Tag ID: " + tag_ID, Toast.LENGTH_SHORT).show();
+            AlertDialog.Builder alert = new AlertDialog.Builder(self);
+            alert.setTitle(getResources().getString(R.string.dialog_title_check_in));
+
+            LayoutInflater inflater;
+            inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LinearLayout ll = (LinearLayout) inflater.inflate(R.layout.check_in_dialog ,null);
+
+            alert.setView(ll);
+            final RadioGroup rg1=(RadioGroup)ll.findViewById(R.id.durations_rg);
+
+            alert.setPositiveButton("Check in!", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+//                Log.d(TAG, "activity: " + activity.getClass());
+//                Log.d(TAG, "rg: " + rg1);
+                    if(rg1.getCheckedRadioButtonId()!=-1){
+                        int id= rg1.getCheckedRadioButtonId();
+                        View radioButton = rg1.findViewById(id);
+                        int radioId = rg1.indexOfChild(radioButton);
+                        RadioButton btn = (RadioButton) rg1.getChildAt(radioId);
+                        String selection = (String) btn.getText();
+                        Toast.makeText(self, "You selected " + selection + " minutes.", Toast.LENGTH_SHORT).show();
+
+                        //Add nameValuePair for http request
+                        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                        String param_name;
+                        String param_value;
+
+                        param_name= "user_id";
+                        param_value= "zhuliang";
+                        nameValuePairs.add(new BasicNameValuePair(param_name, param_value));
+                        param_name= "tag_id";
+                        param_value= "tag_ID";
+                        nameValuePairs.add(new BasicNameValuePair(param_name, param_value));
+                        param_name= "duration";
+                        param_value= selection;
+                        nameValuePairs.add(new BasicNameValuePair(param_name, param_value));
+
+                        // Creating service handler class instance
+                        sh = new ServiceHandler();
+
+                        String json = sh.makeServiceCall(CHECK_IN_URL, ServiceHandler.GET, nameValuePairs);
+                    }
+                }
+            });
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
         }
     }
 
